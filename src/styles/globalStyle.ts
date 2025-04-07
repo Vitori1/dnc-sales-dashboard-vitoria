9bb63708e904ec2c31e197a649dfbefd573c5ebc
Vitori1 <vitoriadagmar@gmail.com>
2025-04-07 20:01:50 -0300

    Usando o Theme

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,7 +1,8 @@
+import { Theme } from '@types'
 import { createGlobalStyle } from 'styled-components'
-import { DefaultTheme } from 'styled-components'
+//import { DefaultTheme } from 'styled-components'
 
-export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
+export const GlobalStyle = createGlobalStyle<{ theme?: Theme }>`
     body, html {
         background: ${(props) => props.theme.appBackground};
         color: ${(props) => props.theme.appColor};
