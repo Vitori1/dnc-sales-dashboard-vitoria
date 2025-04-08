5548b7b880ba8e92f88fbf976212aa46068313cf
Vitori1 <vitoriadagmar@gmail.com>
2025-04-08 19:38:49 -0300

    Style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { BannerImage } from '@/components'
+import { BannerImage, FormComponent } from '@/components'
 import { Box, Container, Grid } from '@mui/material'
 
 function Login() {
@@ -9,6 +9,24 @@ function Login() {
           <Grid sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}>
             <Container maxWidth="sm">
               <h1>LOGIN</h1>
+              <FormComponent
+                inputs={[
+                  { type: 'email', placeholder: 'Email', disabled: true },
+                  { type: 'password', placeholder: 'Senha' },
+                ]}
+                buttons={[
+                  {
+                    className: 'primary',
+                    type: 'submit',
+                    children: 'Login',
+                    disabled: true,
+                  },
+                ]}
+                message={{
+                  msg: 'Sucesso!!!',
+                  type: 'success',
+                }}
+              />
             </Container>
           </Grid>
           <Grid sx={{ display: { xs: 'none', sm: 'block' } }}>
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,5 +1,5 @@
 import { Box, Container, Grid } from '@mui/material'
-import { BannerImage } from '@/components'
+import { BannerImage, FormComponent } from '@/components'
 // item xs={12} sm={6}// item sm={6}
 function Registration() {
   return (
@@ -9,6 +9,19 @@ function Registration() {
           <Grid sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}>
             <Container maxWidth="sm">
               <h1>CADASTRO</h1>
+              <FormComponent
+                inputs={[
+                  { type: 'email', placeholder: 'Email' },
+                  { type: 'password', placeholder: 'Senha' },
+                ]}
+                buttons={[
+                  { className: 'primary', type: 'submit', children: 'Login' },
+                ]}
+                message={{
+                  msg: 'ERRO!!!',
+                  type: 'error',
+                }}
+              />
             </Container>
           </Grid>{' '}
           <Grid sx={{ display: { xs: 'none', sm: 'block' } }}>
