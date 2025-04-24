37d573d363c3d7d212ad53e701f7c6735455cb45
Vitori1 <vitoriadagmar@gmail.com>
2025-04-24 14:01:55 -0300

    Feat: Login page requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { Home, Leads, Login, Profile, Registration } from './pages'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from 'react-router-dom'
 function App() {
+  const ProtectedRoute = () => {
+    const checkAuthCookie = Cookies.get('Authorization')
+    if (!checkAuthCookie) {
+      alert('Autenticação necessária')
+      return <Navigate to="/" replace />
+    }
+    return <Outlet />
+  }
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Registration />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/perfil" element={<Profile />} />
-        <Route path="/leads" element={<Leads />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/perfil" element={<Profile />} />
+          <Route path="/leads" element={<Leads />} />
+        </Route>
       </Routes>
     </Router>
   )
