c131d08196bd42ea7058d49e7fe2fa200eae9be6
Vitori1 <vitoriadagmar@gmail.com>
2025-05-02 10:27:48 -0300

    Feat: Create profile flow

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,3 +1,6 @@
+import { ChangeEvent, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+//component
 import { Box, Container, Grid } from '@mui/material'
 import {
   BannerImage,
@@ -8,8 +11,68 @@ import {
   Logo,
 } from '@/components'
 import { pxToRem } from '@/utils'
+//hooks
+import { useFormValidation, usePost } from '@/hooks'
+//redux
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/redux/slices/index'
+import { setMessage, setProfileData } from '@/redux/slices/createProfile'
+//types
+import { CreateProfileData, InputProps } from '@/types'
 // item xs={12} sm={6}// item sm={6}
 function Registration() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { email } = useSelector((state: RootState) => state.createProfile)
+  const { data, loading, error, postData } = usePost<String, CreateProfileData>(
+    'profile/create'
+  )
+  //form step1
+  const step1Inputs: InputProps[] = [
+    { name: 'name', type: 'text', placeholder: 'Nome', required: true },
+    { name: 'email', type: 'email', placeholder: 'Email' },
+    { name: 'phone', type: 'tel', placeholder: 'Telefone', required: true },
+  ]
+  const handleStep1 = (e: React.FormEvent) => {
+    e.preventDefault()
+    dispatch(
+      setProfileData({
+        email: String(step1FormValues[1]),
+      })
+    )
+  }
+  const {
+    formValues: step1FormValues,
+    formValid: step1FormValid,
+    handleChange: step1FormHandleChange,
+  } = useFormValidation(step1Inputs)
+  //form step2
+  const step2Inputs: InputProps[] = [{ type: 'password', placeholder: 'Senha' }]
+  const handleStep2 = async (e: React.FormEvent) => {
+    e.preventDefault()
+    await postData({
+      name: String(step1FormValues[0]),
+      email: String(step1FormValues[1]),
+      phone: String(step1FormValues[2]),
+      password: String(step2FormValues[0]),
+    })
+  }
+  const {
+    formValues: step2FormValues,
+    formValid: step2FormValid,
+    handleChange: step2FormHandleChange,
+  } = useFormValidation(step2Inputs)
+  const handleStepInputs = email ? step2Inputs : step1Inputs
+  useEffect(() => {
+    if (data !== null) {
+      dispatch(setMessage('Usuário criado com sucesso.'))
+      navigate('/')
+    } else if (error) {
+      alert(
+        `Não foi possível realizar a operação. Entre em contato com o nosso suporte (${error}).`
+      )
+    }
+  }, [data, error, navigate])
   return (
     <>
       <Box>
@@ -20,30 +83,56 @@ function Registration() {
                 <Logo height={41} width={100} />
               </Box>
               <Box sx={{ marginBottom: pxToRem(24) }}>
-                <StyledH1>Faça seu cadastro</StyledH1>
-                <StyledP>Primeiro, diga-nos quem você é.</StyledP>
-                <StyledUl>
-                  <li>Entre 8 e 16 caracteres;</li>
-                  <li>Pelo menos uma letra maiúscula;</li>
-                  <li>Pelo menos um caractere especial.</li>
-                  <li>Pelo menos um número</li>
-                </StyledUl>
+                <StyledH1>
+                  {email ? 'Defina sua senha' : 'Faça seu cadastro'}
+                </StyledH1>
+                <StyledP>
+                  {' '}
+                  {email
+                    ? 'Sua senha deve ter:'
+                    : 'Primeiro, diga-nos quem você é.'}
+                </StyledP>
+                {email && (
+                  <StyledUl>
+                    <li>Entre 8 e 16 caracteres;</li>
+                    <li>Pelo menos uma letra maiúscula;</li>
+                    <li>Pelo menos um caractere especial.</li>
+                    <li>Pelo menos um número</li>
+                  </StyledUl>
+                )}
               </Box>
               <FormComponent
-                inputs={[
-                  { type: 'email', placeholder: 'Email' },
-                  { type: 'password', placeholder: 'Senha' },
-                ]}
+                inputs={handleStepInputs.map((input, index) => ({
+                  type: input.type,
+                  placeholder: input.placeholder,
+                  value: email
+                    ? step2FormValues[index] || ''
+                    : step1FormValues[index] || '',
+                  onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                    email
+                      ? step2FormHandleChange(
+                          index,
+                          (e.target as HTMLInputElement).value
+                        )
+                      : step1FormHandleChange(
+                          index,
+                          (e.target as HTMLInputElement).value
+                        ),
+                }))}
                 buttons={[
-                  { className: 'primary', type: 'submit', children: 'Login' },
+                  {
+                    className: 'primary',
+                    disabled: email
+                      ? !step2FormValid || loading
+                      : !step1FormValid,
+                    onClick: email ? handleStep2 : handleStep1,
+                    type: 'submit',
+                    children: email ? 'Enviar' : 'Próximo',
+                  },
                 ]}
-                message={{
-                  msg: 'ERRO!!!',
-                  type: 'error',
-                }}
               />
             </Container>
-          </Grid>{' '}
+          </Grid>
           <Grid sx={{ display: { xs: 'none', sm: 'block' } }}>
             <BannerImage />
           </Grid>
--- src/types/profileData.d.ts
+++ src/types/profileData.d.ts
@@ -5,3 +5,10 @@ export interface ProfileEditableData {
 export interface ProfileData extends ProfileEditableData {
   email: string
 }
+export interface CreateProfileData {
+  name: string
+  email: string
+  phone: string
+  password: string
+  message?: string | null
+}
